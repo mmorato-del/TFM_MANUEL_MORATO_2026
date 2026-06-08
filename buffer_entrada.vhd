1bcc3643e13e3ad2f4422e3f7a5fc10161ea72d7
mmorato <mmorato@unizar.es>
2026-06-08 11:35:19 +0200

    Clean up buffer_entrada.vhd
    Removed commented code and unnecessary blank lines.

--- buffer_entrada.vhd
+++ buffer_entrada.vhd
@@ -1,8 +1,6 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
-
 
 entity FFT_buffer is
   Generic (
@@ -14,8 +12,7 @@ entity FFT_buffer is
     i_clk : in std_logic;
     i_word_valid : in std_logic;
     i_word : in std_logic_vector(N_chan*N_bits-1 downto 0);
-    
-    
+  
     o_tvalid : out std_logic;
     o_tlast : out std_logic;
     o_word : out std_logic_vector(N_bits-1 downto 0)
@@ -35,7 +32,6 @@ begin
             -- Proceso de lectura, cuando se recibe un dato.
             if i_word_valid = '1' then
                 -- Entra una palabra nueva en el registro.
-                --FFT_shift_register <= FFT_shift_register(FFT_size-2 downto 0) & i_word; CRUCE INCORRECTO DE TO CON DOWNTO
                 FFT_shift_register <= i_word & FFT_shift_register( 0 to FFT_size-2);
                  -- Comienza la transmisión de los 256 puntos del primer canal
                 trans_enable <= '1';
@@ -63,13 +59,6 @@ begin
                     FFT_points_count <= FFT_points_count +1;
                     o_tlast <= '0';
                 end if;  
-                
-                -- Gestión de tlast. No se puede poner en el bucle anterior porque tiene que ir con el último punto, no con el primero del siguiente.
-                --if FFT_points_count = FFT_size-1 then
-                --    o_tlast <= '1';
-                --else
-                --    o_tlast <= '0';
-                --end if;
                 
             else -- SI NO ESTÁ HABILITADA LA TRANSMISION
                 o_tlast <= '0';
